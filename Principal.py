6d8829b7d37afe9933541e30f61bad4871832852
lucas <romancini12@gmail.com>
2022-02-23 01:37:23 -0300

    commit on Comanda2.0

--- Principal.py
+++ Principal.py
@@ -1,6 +1,5 @@
 from ast import Pass
 from asyncio.windows_events import NULL
-import encodings
 from random import sample
 from telnetlib import ENCRYPT
 from PyQt5.QtGui import * 
@@ -29,15 +28,28 @@ inicio.label.setPixmap(logo)
 #globais
 
 erro = 0
-saveuserr = False
+saveuserr = True
+abremesa = 0
 estilovermelho = """QLineEdit{border:2px solid rgb(255,0,0);border-radius:15px}
         QLineEdit:hover{border:4px solid rgb(255,0,0);border-radius:15px}"""
 estilonormal = """QLineEdit{color:rgb(141, 141, 141);border:2px solid rgb(255, 212, 0);border-radius:20px}
 QLineEdit:hover{border:3px solid rgb(220, 180, 0)}"""
 
+### SALVA NO CAMPO O USUARIO
+def saveuser():
+    global saveuserr
+    check = login.checkBox.isChecked()
+    saveuserr = check
+    if check == True:
+        saveuserr = True
+    else:
+        saveuserr = False
+### STARTA O PROGRAMA
 def abretelalogin():
     global erro
+    global saveuserr
     login.show()
+    print(saveuserr)
     try:
         from conexao import conexao
         conexao()
@@ -52,31 +64,9 @@ def abretelalogin():
         None
     else:
         login.lineEdit.setText(str(dado[0][0]))
-        login.checkBox.setChecked(True)
+    
 
-def validalogin():
-    global erro
-    from conexao import con
-    if erro == 1:
-        QMessageBox.about(login,'login','SEM CONEXÃO COM BANCO DE DADOS !')
-    else:
-        cur = con.cursor()
-        username = login.lineEdit.text()
-        cur.execute("SELECT password FROM usuarios WHERE user = '{}'".format(str(username)))
-        user = cur.fetchall()
-        if str(user) == '[]':
-            login.label_3.setText('  USUARIO NÃO EXISTE  ')
-            login.label_3.setStyleSheet('QLabel{color:rgb(255,0,0);border:2px solid rgb(255,0,0); border-radius:10px}')
-        else:
-            senha_banco = user[0][0]
-            password = login.lineEdit_2.text()
-            if senha_banco == password:
-                login.close()
-                inicio.show()
-            else:
-                login.label_3.setText("VERIFIQUE OS DADOS")
-
-
+##registro
 def abreregistro():
     login.close()
     registro.show()
@@ -85,7 +75,6 @@ def registra():
 
     #CONEXAO COM OS BANCO DE DADOS
     from conexao import con
-    global key
     user = registro.user.text()
     password = registro.password.text()
     email = registro.email.text()
@@ -137,16 +126,7 @@ def registra():
         registro.label_3.setStyleSheet('color:rgb(255,0,0)')
 
     ## REGISTRO ALTERAÇOES VISUAIS
-
-
-def saveuser():
-    global saveuserr
-    check = login.checkBox.isChecked()
-    if check == True:
-        saveuserr = True
-    else:
-        saveuserr = False
-
+#VALIDA O LOGIN
 def validalogin():
     global erro
     global saveuserr
@@ -162,11 +142,19 @@ def validalogin():
             login.label_3.setText('  USUARIO NÃO EXISTE  ')
             login.label_3.setStyleSheet('QLabel{color:rgb(255,0,0);border:2px solid rgb(255,0,0); border-radius:10px}')
         else:
-            if saveuserr == True:
+            cur.execute("SELECT nome FROM saveuser WHERE nome ='{}'".format(username))
+            dado = cur.fetchall()
+            if str(dado) == '[]' and saveuserr == True:
                 cur.execute("INSERT INTO saveuser (nome) VALUES ('{}')".format(username))
                 con.commit()
+            elif str(dado) != '[]' and saveuserr == True:
+                pass
+            elif str(dado) != '[]' and saveuserr == False:
+                cur.execute("DELETE FROM saveuser WHERE nome='{}'".format(username))
+                login.lineEdit.setText('')
+                con.commit()
             else:
-                Pass
+                None
             senha_banco = user[0][0]
             senha_banco_bytes = senha_banco.encode("ascii")
             base64_string = base64.b64decode(senha_banco_bytes)
@@ -178,7 +166,7 @@ def validalogin():
                 inicio.show()
             else:
                 login.label_3.setText("VERIFIQUE OS DADOS")
-
+#ALTERA VISIBILIDADE DA SENHA
 def olhosenha():
     registro.olhosenha.setCheckable(True)
     if registro.olhosenha.isChecked():
@@ -190,14 +178,33 @@ def olhosenha():
         registro.c_password.setEchoMode(registro.password.Normal)
         registro.olhosenha.setIcon(QIcon(QPixmap('icones/aberto.png')))
 
+def abremesas():
+    None
+
+def addmesas():
+    None
+    
 abretelalogin()
 
+
+#LOGIN
 login.pushButton.clicked.connect(validalogin)
 login.pushButton_2.clicked.connect(abreregistro)
+login.checkBox.stateChanged.connect(saveuser)
+
+#REGISTROS
 registro.pushButton_2.clicked.connect(registra)
 registro.olhosenha.clicked.connect(olhosenha)
 registro.password.setEchoMode(registro.password.Password)
 registro.c_password.setEchoMode(registro.password.Password)
-login.checkBox.stateChanged.connect(saveuser)
 registro.olhosenha.setIcon(QIcon(QPixmap('icones/fechado.png')))
+
+
+##INICIOS
+inicio.mesas.clicked.connect(abremesas)
+inicio.adicionar.clicked.connect(addmesas)
+
+
+
+
 app.exec()
